758e071be3ccbf92ae49441ea1f9c56f4425ab58
Jimaine <jimaine@outlook.de>
2021-05-02 19:46:49 +0200

    Proceed CatalogApi
    Generate DataLayer

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            string connectionString = configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+            string databaseName = configuration.GetValue<string>("DatabaseSettings:DatabaseName");
+            string collectionName = configuration.GetValue<string>("DatabaseSettings:CollectionName");
+
+            var mongoClient = new MongoClient(connectionString);
+            var database = mongoClient.GetDatabase(databaseName);
+
+            Products = database.GetCollection<Product>(collectionName);
+            CatalogContextSeed.SeedData(Products);
+        }
+    }
+}
